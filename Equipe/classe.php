8bde0a3b9dfc8f4a1019d87f0d71708baaded2f5
koyppa <50912955+koyppa@users.noreply.github.com>
2019-05-23 15:04:46 +0200

    Add files via upload

--- Equipe/classe.php
+++ Equipe/classe.php
@@ -9,11 +9,7 @@ class Classe
 	private $force;
 	private $qi;
 	
-	function __construct()
-	{
-
-	}	
-	//Type
+	
 	function getType()
 	{
 		return $this->type;
@@ -22,45 +18,5 @@ class Classe
 	function setType($type)
 	{
 		$this->type=$type;
-	}
-	//Hp
-	function getHp()
-	{
-		return $this->hp;
-	}
-
-	function setHp($hp)
-	{
-		$this->hp=$hp;
-	}
-	//Magie
-	function getMagie()
-	{
-		return $this->magie;
-	}
-
-	function setMagie($magie)
-	{
-		$this->magie=$magie;
-	}
-	//Force
-	function getForce()
-	{
-		return $this->force;
-	}
-
-	function setForce($force)
-	{
-		$this->force=$force;
-	}
-	//QI
-	function getQi()
-	{
-		return $this->qi;
-	}
-
-	function setQi($qi)
-	{
-		$this->qi=$qi;
 	}
 }
