a6a88722373b14511c6a750c883a095d956b3085
CocoBac <49440351+CocoBac@users.noreply.github.com>
2019-05-23 16:50:43 +0200

    Test des évènements sur principal.php

--- principal.php
+++ principal.php
@@ -17,22 +17,55 @@ require_once(__DIR__.'/Equipe/affichage.php');
 	<br>
 	
 	<?php
+
 	$s = new Scenario;
+	$s->addEtape("HpMage","ForceCombattant");
+	$s->getTabEtapes()[1]->addChoix("Hp Mage baisse de 25");
+	$s->getTabEtapes()[2]->addChoix("Force Combattant baisse de 90");
+	$s->getTabEtapes()[2]->addChoix("Force Combattant gagne 10");
+
 	$e = new Equipe;
 	$e->addJoueur("J1");
+	$e->addJoueur("J2");
 	$m = new Mage;
+	$c = new Combattant;
 	$e->getTabJoueurs()[0]->setPerso($m);
-	echo $e->getTabJoueurs()[0]->getPerso()->getHp();
+	$e->getTabJoueurs()[1]->setPerso($c);
+	
+	echo "Mage Hp : ", $e->getTabJoueurs()[0]->getPerso()->getHp();
+	
+	$dhp = new eventDamageHp($e);
+	$dhp->setCible(0);
+	$dhp->setDamage(25, "-");
+	$s->getTabEtapes()[1]->getTabChoix()[1]->addEvent($dhp);
+	$s->getTabEtapes()[1]->getTabChoix()[1]->exeEvent($e, 0);
 
-	$d = new eventDamageHp;	
-	$d->setCible(0);
-	$d->setDamage($d->getCurrent($e)- 25);
-	$d->exe($e);
-	echo $e->getTabJoueurs()[0]->getPerso()->getHp();
+	echo " -> ", $e->getTabJoueurs()[0]->getPerso()->getHp();
 
-	$t = new EventTexte;
-	$t->setTexte("<br>Bonjour voyageur<br>Bienvenue dans le village des mages<br><br>Es-tu un mage un toi-même ?");
-	$t->exe();
+	echo "<br>Combattant Force : ", $e->getTabJoueurs()[1]->getPerso()->getForce();
+	
+	$df = new eventDamageForce($e);
+	$df->setCible(1);
+	$df->setDamage(90, "-");
+	$s->getTabEtapes()[2]->getTabChoix()[1]->addEvent($df);
+
+	$pf = new eventDamageForce($e);
+	$pf->setCible(1);
+	$pf->setDamage(10, "+");
+	$s->getTabEtapes()[2]->getTabChoix()[1]->addEvent($pf);
+
+	$s->getTabEtapes()[2]->getTabChoix()[1]->exeEvent($e);
+	echo " -> ", $e->getTabJoueurs()[1]->getPerso()->getForce();
+
+
+
+
+
+
+
+
+
+
 
 	?>
 
